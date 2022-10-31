b84795985b767d645f07500bb85cf26d3acd7c1d
kebin20 <ktanzyl@hotmail.com>
2022-10-31 16:51:52 +0900

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <button className="footer--social-media-btn">
+        <i className="fa-brands fa-twitter"></i>
+      </button>
+      <button className="footer--social-media-btn">
+        <i className="fa-brands fa-facebook"></i>
+      </button>
+      <button className="footer--social-media-btn">
+        <i className="fa-brands fa-instagram"></i>
+      </button>
+      <button className="footer--social-media-btn">
+        <i className="fa-brands fa-github"></i>
+      </button>
+    </footer>
+  );
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Info() {
+  return (
+    <header>
+      <img src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.6435-9/64704466_10162351294200497_4832434912101400576_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=vy4qa6azUvQAX_ji2vu&_nc_ht=scontent-nrt1-1.xx&oh=00_AfAQkotVy4qeRrmXPjGC_ocsVONRfvdZ7h1Iu0UrapILZw&oe=63806939" className="Info--profile-pic"></img>
+      <div className="top-info">
+        <h2>Kevin Tanzyl</h2>
+        <h5>Frontend Developer</h5>
+        <p>kebin20.io</p>
+      </div>
+      <div className="Info--button-row">
+        <button className="Info--contact-btn email">
+          <i className="fa-solid fa-envelope"></i>Email
+        </button>
+        <button className="Info--contact-btn linkedin">
+          <i className="fa-brands fa-linkedin"></i>LinkedIn
+        </button>
+      </div>
+    </header>
+  );
+}
